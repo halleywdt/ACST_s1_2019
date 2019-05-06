c27e459d1047afd8aa3ab69c69c205b975d92355
halleywdt <48709608+halleywdt@users.noreply.github.com>
2019-05-06 22:27:52 +1000

    Delete q1.py

--- q1.py
+++
@@ -1,5 +0,0 @@
-with open('C:/Users/10142/OneDrive/desktop/ACST890/as1/twain.txt','r',encoding='utf-8') as f: 
- contents=f.read()  
-correction=contents.replace('Huck','HucK') 
-with open('C:/Users/10142/OneDrive/desktop/ACST890/as1/twain.txt', 'w', encoding='utf-8') as w: 
- w.write(correction) 
